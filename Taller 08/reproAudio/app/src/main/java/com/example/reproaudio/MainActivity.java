5eaad803d9f48255a1d71df4b3e30c79c7d96d56
Stephasmc <stephasmc@gmail.com>
2023-05-07 22:43:38 -0500

    Video-Audio

---
+++ Taller 08/reproAudio/app/src/main/java/com/example/reproaudio/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.reproaudio;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.media.MediaPlayer;
+import android.view.View;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void presionGato(View v) {
+        MediaPlayer mp = MediaPlayer.create(this, R.raw.gato);
+        mp.start();
+    }
+
+    public void presionLeon(View v) {
+        MediaPlayer mp = MediaPlayer.create(this, R.raw.leon);
+        mp.start();
+    }
+}
